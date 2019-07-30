d1ded4a2897aa4a7600c6cdfae38658de8f97cf0
ngill28 <35106903+ngill28@users.noreply.github.com>
2019-07-30 14:16:13 -0500

    inital project load into git repository

---
+++ src/components/users/Search.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Search extends Component {
+  state = {
+    text: ''
+  };
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className="form">
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.onChange}
+            name="text"
+            placeholder="Search Users..."
+          />
+          <input
+            type="submit"
+            value="Submit"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
